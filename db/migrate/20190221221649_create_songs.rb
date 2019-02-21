6e66514ca80c002b920720399585d2781f1e88e3
AJ Harmond <ajharmond@gmail.com>
2019-02-21 15:17:37 -0700

    Working billboard/artist CRUD actions, beginning of song model.

---
+++ db/migrate/20190221221649_create_songs.rb
@@ -0,0 +1,14 @@
+class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :billboard_rank
+      t.string :album
+      t.integer :year
+      t.belongs_to :artist, foreign_key: true
+      t.belongs_to :board, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
